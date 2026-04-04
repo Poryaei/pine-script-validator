bd4220f1522b8f97488f8540f1938e38b05c1b01
Abolfazl Poryaei <poryaeiabolfazl@gmail.com>
2026-04-04 10:56:17 +0300

    Tighten consistency warnings to match Pine

--- src/pinescript_validator/ast_validator.py
+++ src/pinescript_validator/ast_validator.py
@@ -13,19 +13,12 @@ CONSISTENCY_SENSITIVE_BUILTINS = frozenset(
         "ta.cross",
         "ta.crossover",
         "ta.crossunder",
-        "ta.ema",
         "ta.highest",
         "ta.lowest",
         "ta.highestbars",
         "ta.lowestbars",
-        "ta.sma",
         "ta.stdev",
         "ta.variance",
-        "ta.rma",
-        "ta.wma",
-        "ta.vwma",
-        "ta.swma",
-        "ta.linreg",
     }
 )
 
